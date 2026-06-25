3548d4cfac6bbfc3bf4aa9c85fe9828c21a56eee
Usmanov009 <chatclaude700@gmail.com>
2026-06-25 14:13:46 +0500

    Fix frontend dependency declarations, update router imports, and add TypeScript config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider } from "react-router";
+import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import "./i18n";
 import { useTelegramWebApp } from "./hooks/useTelegramWebApp";
